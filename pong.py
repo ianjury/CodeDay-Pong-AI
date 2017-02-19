19edbac8d8d64686b726b937ee27802e2c831fde
Ian Jury <mrianjury@gmail.com>
2017-02-18 21:21:27 -0800

    Fixed bug where ball would get stuck without x axis momentum

--- pong.py
+++ pong.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 #
 #       This program is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
@@ -20,6 +19,9 @@ pygame.init()
 screen=pygame.display.set_mode((640,480),0,32)
 pygame.display.set_caption("Pong Simulator")
 
+#Change this to manipulate speed of simulation
+GLOBAL_SPEED = 1000
+
 #Creating 2 bars, a ball and background.
 back = pygame.Surface((640,480)) #grid
 background = back.convert()
@@ -34,18 +36,24 @@ circ = pygame.draw.circle(circ_sur,(0,255,0),(15/2,15/2),15/2) #surface, color, 
 circle = circ_sur.convert()
 circle.set_colorkey((0,0,0))
 
-# some definitions
+# x and y coordinates / speeds
 bar1_x, bar2_x = 10. , 620.
 bar1_y, bar2_y = 215. , 215.
 circle_x, circle_y = 307.5, 232.5
 bar1_move, bar2_move = 0. , 0.
-speed_x, speed_y, speed_circ = 250., 250., 250.
+speed_x, speed_y, speed_circ = GLOBAL_SPEED, GLOBAL_SPEED, GLOBAL_SPEED
 
 #clock and font objects
 clock = pygame.time.Clock()
 font = pygame.font.SysFont("calibri",40)
 
 while True: #game logic
+    #Gives ability to exit
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            exit()
+
+    #redraws game state
     screen.blit(background,(0,0))
     frame = pygame.draw.rect(screen,(255,255,255),Rect((5,5),(630,470)),2)
     middle_line = pygame.draw.aaline(screen,(255,255,255),(330,5),(330,475))
@@ -56,42 +64,47 @@ while True: #game logic
     #movement of circle
     time_passed = clock.tick(30)
     time_sec = time_passed / 1000.0
-
+    #time_sec = .9
     circle_x += speed_x * time_sec
     circle_y += speed_y * time_sec
-    #ai_speed = speed_circ * time_sec
-    ai_speed = 1
+    ai_speed = speed_circ * time_sec
+    #ai_speed = 1
 
     #right side AI
     bar2_y = circle_y * (random.randrange(9, 10) * .1);
-
     #Left Side AI position
     bar1_y = circle_y * (random.randrange(9, 10) * .1);
 
-    #Collision
+    #Collision for left side
     if circle_x <= bar1_x + 8:
         if circle_y >= bar1_y - 7.5 and circle_y <= bar1_y + 42.5:
-            circle_x = 20.
-            # TODO add randomness here
-            speed_x = -speed_x
+            circle_x = 20
+            speed_x = -speed_x * (random.randrange(9, 15) * .1)
+            if speed_x == 0: #in case the ball gets stuck moving up and down
+                    speed_x = GLOBAL_SPEED / 2
+
+    # Collision for right side
     if circle_x >= bar2_x - 17:
         if circle_y >= bar2_y - 7.5 and circle_y <= bar2_y + 42.5:
-            circle_x = 605.
-            # TODO add randomness here
-            speed_x = -speed_x
+            circle_x = 605
+            speed_x = -speed_x * (random.randrange(9, 15) * .1)
+            if speed_x == 0: #in case the ball gets stuck moving up and down
+                    speed_x = (GLOBAL_SPEED / 2) * -1
 
     #Increments score
     if circle_x < 5.:
+        print "RIGHT AI WINS"
         exit()
     elif circle_x > 620.:
+        print "LEFT AI WINS"
         exit()
 
     #Constrains vertical range of ball
     if circle_y <= 10.:
-        speed_y = -speed_y
+        speed_y = -speed_y * (random.randrange(9, 10) * .1)
         circle_y = 10.
     elif circle_y >= 457.5:
-        speed_y = -speed_y
+        speed_y = -speed_y * (random.randrange(9, 10) * .1)
         circle_y = 457.5
 
-    pygame.display.flip()
+    pygame.display.update()
